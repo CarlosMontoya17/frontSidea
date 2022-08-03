481d1be955758cb644af05c3665fe6fd17bc7aaa
Carlos Montoya <carlos.programacion@gruporyc.com.mx>
2022-08-02 22:02:26 -0500

    Token

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,9 +7,11 @@ import { AuthService } from 'src/app/services/auth/auth.service';
 //Icons
 import { faFaceGrin } from '@fortawesome/free-solid-svg-icons';
 import { faPowerOff } from '@fortawesome/free-solid-svg-icons';
+import { faAddressCard } from '@fortawesome/free-solid-svg-icons';
+import { faKey } from '@fortawesome/free-solid-svg-icons';
 //Models
 import { myData } from 'src/app/models/myData.model';
-
+import { Robot } from 'src/app/models/Robot.model';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -21,12 +23,16 @@ export class HomeComponent implements OnInit {
   //Icons
   faFaceGrin = faFaceGrin;
   faPowerOff = faPowerOff;
+  faAddressCard = faAddressCard;
+  faKey = faKey;
   //Captha Image
   imageToShow: any;
   myRol:string = "";
-
-
-  constructor(private localStorageService: LocalstorageService, private robots: RobotsService, auth: AuthService) {
+  robothandle:any = [];
+  //new Instruction
+  instruction:any = [];
+  NewAccessToken:string="";
+  constructor(private localStorageService: LocalstorageService, private robots: RobotsService, private auth: AuthService) {
     this.myData$ = auth.GetMyData;
   }
 
@@ -35,17 +41,20 @@ export class HomeComponent implements OnInit {
       this.myRol = data.rol;
       switch (data.rol) {
         case "Admin":
-          this.GiveMeCaptcha();
+          this.GiveMeAllRobots();
           break;
         case "Supervisor":
-          this.GiveMeCaptcha();
           break;
       }
     });
-
   }
 
 
+  GiveMeAllRobots(){
+    this.robots.GetAllRobots().subscribe(data => {
+      this.robothandle = data;
+    });
+  }
 
   GiveMeCaptcha() {
     this.robots.Captcha("robot2").subscribe(data => {
@@ -53,6 +62,80 @@ export class HomeComponent implements OnInit {
     }, err => {
       // console.log("error");
     });
+  }
+
+  Instruction(instruction:string, robot:Robot){
+    switch(instruction){
+      case "off":
+        this.instruction = [instruction, robot];
+        document.getElementById("modal")?.setAttribute("style", "display: block;");
+        document.getElementById("confirmRequest")?.setAttribute("style", "display: block;");
+        break;
+      case "on":
+        this.instruction = [instruction, robot];
+        document.getElementById("modal")?.setAttribute("style", "display: block;");
+        document.getElementById("confirmRequest")?.setAttribute("style", "display: block;");
+        break;
+      case "changeAccessToken":
+        this.instruction = [instruction, robot];
+        document.getElementById("modal")?.setAttribute("style", "display: block;");
+        document.getElementById("changeToken")?.setAttribute("style", "display: block;");
+        break;
+    }
+  }
+
+  NextStep(){
+    switch(this.instruction[0]){
+      case "changeAccessToken":
+        document.getElementById("changeToken")?.setAttribute("style", "display: none;");
+        document.getElementById("confirmRequest")?.setAttribute("style", "display: block;");
+        break;
+    }
+  }
+
+  Send(){
+    switch(this.instruction[0]){
+      case "off":
+        this.robots.UpNewInstruction(this.instruction[0], this.instruction[1].name).subscribe(data => {
+          console.log(data);
+          this.instruction = [];
+          document.getElementById("modal")?.setAttribute("style", "display: none;");
+          document.getElementById("confirmRequest")?.setAttribute("style", "display: none;");
+          this.GiveMeAllRobots();
+        }, err => {
+          this.auth.Unauth();
+        });
+
+        break;
+      case "on":
+        this.robots.UpNewInstruction(this.instruction[0], this.instruction[1].name).subscribe(data => {
+          console.log(data);
+          this.instruction = [];
+          document.getElementById("modal")?.setAttribute("style", "display: none;");
+          document.getElementById("confirmRequest")?.setAttribute("style", "display: none;");
+          this.GiveMeAllRobots();
+        }, err => {
+          this.auth.Unauth();
+        });
+        break;
+      case "changeAccessToken":
+        this.robots.UpNewInstruction(`${this.instruction[0]}=${this.NewAccessToken}`, this.instruction[1].name).subscribe(data => {
+          this.instruction = [];
+          this.NewAccessToken = "";
+          document.getElementById("modal")?.setAttribute("style", "display: none;");
+          document.getElementById("confirmRequest")?.setAttribute("style", "display: none;");  
+          this.GiveMeAllRobots();      
+        }, err => {
+          this.auth.Unauth();
+        });
+        break;
+    }
+  }
+
+  CancelModal(){
+    this.instruction = [];
+    document.getElementById("modal")?.setAttribute("style", "display: none;");
+    document.getElementById("confirmRequest")?.setAttribute("style", "display: none;");
   }
 
 
@@ -66,5 +149,8 @@ export class HomeComponent implements OnInit {
       reader.readAsDataURL(image);
     }
   }
+
+
+
 
 }
--- src/app/services/robots.service.ts
+++ src/app/services/robots.service.ts
@@ -16,5 +16,17 @@ export class RobotsService {
     return this.http.get(api+"/api/robots/captcha/view/"+robotname, { responseType: 'blob', headers });
   }
 
+  GetAllRobots(){
+    let token = this.local.TokenDesencrypt();
+    const headers = new HttpHeaders({ 'x-access-token': token! });
+    return this.http.get(api+"/api/robots/getAll/", { headers });
+  }
+
+  UpNewInstruction(instruction:any, name:any){
+    let token = this.local.TokenDesencrypt();
+    const headers = new HttpHeaders({ 'x-access-token': token! });
+    return this.http.post(`${api}/api/robots/controller/instruction/new/`, {name, instruction}, {headers});
+  }
+
 
 }
