4199a1441091cfa5fb6f969b32401bf28398a369
Carlos Montoya <carlos.programacion@gruporyc.com.mx>
2022-10-05 08:00:17 -0500

    sidea

--- src/app/services/requests/requests.service.ts
+++ src/app/services/requests/requests.service.ts
@@ -28,9 +28,12 @@ export class RequestsService {
   }
 
   SendARequest(type:any, metadata:any, preferences:any){
+    let HANDLE = { data: {type: type, metadata: metadata, preferences: preferences}, source: "actas" };
+
     let token = this.local.TokenDesencrypt();
     const headers = new HttpHeaders({ 'x-access-token': token! });
-    return this.http.post(urlApi+'/api/actas/requests/new/', { type, metadata, preferences },{ headers });
+
+    return this.http.post(urlApi+'/api/robots/services/new/', HANDLE ,{ headers });
   }
 
   DownloadActa(id:any){
@@ -39,9 +42,11 @@ export class RequestsService {
 
   //RFCs
   SendRFCRequest(search:any, data:any, clasification:any){
+    let HANDLE = { data: {search: search, data: data, clasification: clasification}, source: "rfcs" };
     let token = this.local.TokenDesencrypt();
     const headers = new HttpHeaders({ 'x-access-token': token! });
-    return this.http.post(urlApi+'/api/rfc/requests/news/', { search, data, clasification },{ headers });
+
+    return this.http.post(urlApi+'/api/robots/services/new/', HANDLE,{ headers });
   }
 
   getAllRFCRequest(date:any){
